a4cf492592b939dd21f274f23f0ca8c904282907
Jun Nishimura <n.junjun0303@gmail.com>
2021-02-04 10:31:06 +0900

    Update Tokenizer

--- common/sample/sample.py
+++ common/sample/sample.py
@@ -1,6 +1,8 @@
 import MeCab
 
 def main():
+    # unidic format指定
+    # https://unidic.ninjal.ac.jp/faq
     tagger = MeCab.Tagger("-d ../../dic/UniDic-wabun_1603")
     print(tagger.parse("花の顔に晴うてしてや朧月"))
 
